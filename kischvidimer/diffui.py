dnschneid/kischvidimer

commit 8fb0aae536bb94c293d68af75ee943e9e626dc4b

Aggressive lint

kischvidimer/diffui.py
@@ -14,6 +14,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import base64
+import contextlib
 import gc
 import html as html_mod
 import json
@@ -34,7 +35,7 @@ from .kicad_common import Drawable, Variables
 from .svg import Svg
 
 
-class Page(object):
+class Page:
   def __init__(
     self,
     name,
@@ -71,9 +72,9 @@ class Page(object):
       context += proj.context()
     else:
       page.fillvars(variables, diffs, context)
-    self.id = self.svg.vars.get("~pageid", "page%s" % self.svg.getuid(self))
+    self.id = self.svg.vars.get("~pageid", f"page{self.svg.getuid(self)}")
     self.name = ": ".join(
-      (n for n in (name, self.svg.vars.get("~pagetitle")) if n)
+      n for n in (name, self.svg.vars.get("~pagetitle")) if n
     )
     # Common background elements
     page.fillsvg(
@@ -100,8 +101,7 @@ class Page(object):
   def alldiffs(self):
     for diffpair in self.conflicts + self.safediffs:
       for diffs in diffpair:
-        for diff in diffs:
-          yield diff
+        yield from diffs
 
 
 class HTTPHandler(BaseHTTPRequestHandler):
@@ -117,7 +117,7 @@ class HTTPHandler(BaseHTTPRequestHandler):
     self.server.diffui.log(fmt % args)
 
 
-class DiffUI(object):
+class DiffUI:
   """Creates a diff/merge UI and launches it for the user, then updates the base
   with the user-selected diffs.
   """
@@ -162,18 +162,14 @@ class DiffUI(object):
 
   def _mkdtemp(self):
     if not self._tempdir:
-      self._tempdir = tempfile.mkdtemp(
-        prefix="kischvidimer.diffui.%s" % id(self)
-      )
+      self._tempdir = tempfile.mkdtemp(prefix=f"kischvidimer.diffui.{id(self)}")
       with open(os.path.join(self._tempdir, "First Run"), "w"):
         pass
 
   def _cleanup(self):
     if self._uiprocess:
-      try:
+      with contextlib.suppress(OSError):
         self._uiprocess.terminate()
-      except OSError:
-        pass
     if self._tempdir:
       shutil.rmtree(self._tempdir, ignore_errors=True)
       self._tempdir = None
@@ -347,17 +343,15 @@ class DiffUI(object):
 
   def log(self, msg):
     if self._verbosity > 0:
-      sys.stderr.write("%s\n" % msg)
+      sys.stderr.write(f"{msg}\n")
 
   def _icon(self, icon):
     icon = os.path.join(os.path.dirname(__file__), "icons", icon)
     imagetype = (
       "svg+xml" if icon.endswith(".svg") else icon.rpartition(".")[2].lower()
     )
-    return "data:image/%s;base64,%s" % (
-      imagetype,
-      base64.b64encode(open(icon, "rb").read()).decode("ascii"),
-    )
+    b64 = base64.b64encode(open(icon, "rb").read()).decode("ascii")
+    return f"data:image/{imagetype};base64,{b64}"
 
   def _fillsvg(self, svg):
     svg.symbols = self._symbols
@@ -392,12 +386,13 @@ class DiffUI(object):
   @staticmethod
   def loadhtml(path):
     # Loads in an html file and replaces <img> tags with embedded SVG files
-    with open(path, "r", encoding="utf-8") as f:
+    with open(path, encoding="utf-8") as f:
       h = f.read()
 
-    def attrs(tag, overrides={}):
+    def attrs(tag, overrides=None):
       data = {a[1]: a[2] for a in re.finditer(r'\b([a-zA-Z]+)="([^"]+)"', tag)}
-      data.update(overrides)
+      if overrides:
+        data.update(overrides)
       return data
 
     def repl(m):
@@ -405,18 +400,14 @@ class DiffUI(object):
       if "src" not in data:
         return m[0]
       # Load the SVG
-      with open(os.path.join(os.path.dirname(path), data["src"]), "r") as f:
+      with open(os.path.join(os.path.dirname(path), data["src"])) as f:
         s = f.read()
       # Remove "fill" from the icon if not specified in the img tag
       data.setdefault("fill", None)
       data["src"] = None
       # Merge the SVG tag
-      return re.sub(
-        r"<svg\s[^>]*>",
-        lambda m: "<svg%s>"
-        % ("".join(f' {k}="{v}"' for k, v in attrs(m[0], data).items() if v)),
-        s,
-      )
+      inner = "".join(f' {k}="{v}"' for k, v in attrs(m[0], data).items() if v)
+      return re.sub(r"<svg\s[^>]*>", lambda m: f"<svg{inner}>", s)
 
     return re.sub(r"<img\s[^>]*>", repl, h)
 
@@ -452,11 +443,9 @@ class DiffUI(object):
     html.append(
       '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">'
     )
-    html.append(
-      '<link rel="icon" href="%s"/>'
-      % self._icon("%s.svg" % DiffUI.MODE_ICONS[self._mode])
-    )
-    html.append("<title>%s</title></head>" % Svg.escape(window_title))
+    pageicon = self._icon(f"{DiffUI.MODE_ICONS[self._mode]}.svg")
+    html.append(f'<link rel="icon" href="{pageicon}"/>')
+    html.append(f"<title>{Svg.escape(window_title)}</title></head>")
     html.append("<body>")
     srcdir = os.path.realpath(
       os.path.join(os.getcwd(), os.path.dirname(__file__))
@@ -464,7 +453,7 @@ class DiffUI(object):
     # Embed styles
     html.append("<style>")
     for css in ["diffui.css", "js-libraries/material.min.css"]:
-      with open(os.path.join(srcdir, css), "r", encoding="utf-8") as f:
+      with open(os.path.join(srcdir, css), encoding="utf-8") as f:
         html.extend(
           line.strip() for line in f if "sourceMappingURL" not in line
         )
@@ -474,7 +463,7 @@ class DiffUI(object):
     jsdir = os.path.join(srcdir, "js-libraries")
     for lib in os.listdir(jsdir):
       if lib.endswith(".min.js"):
-        with open(os.path.join(jsdir, lib), "r", encoding="utf-8") as f:
+        with open(os.path.join(jsdir, lib), encoding="utf-8") as f:
           html.extend(
             line.strip() for line in f if "sourceMappingURL" not in line
           )
@@ -483,41 +472,29 @@ class DiffUI(object):
     html.append(DiffUI.loadhtml(os.path.join(srcdir, "diffui.html")))
     # Code
     html.append("<script>")
-    html.append('let uiVersion = "%s";' % git.get_version(srcdir))
-    html.append(
-      'let schematicTitle = "%s";'
-      % title[-1].replace("\\", "\\\\").replace('"', '\\"')
-    )
-    html.append(
-      'let schematicVersion = "%s";'
-      % self.ver.replace("\\", "\\\\").replace('"', '\\"')
-    )
-    html.append("let uiMode = %u;" % self._mode)
+    html.append(f'let uiVersion = "{git.get_version(srcdir)}";')
+    safetitle = title[-1].replace("\\", "\\\\").replace('"', '\\"')
+    safevers = self.ver.replace("\\", "\\\\").replace('"', '\\"')
+    html.append(f'let schematicTitle = "{safetitle}";')
+    html.append(f'let schematicVersion = "{safevers}";')
+    html.append(f"let uiMode = {self._mode};")
     if self._mode >= DiffUI.MODE_DIFF:
-      html.append(
-        "let diffIcon = '%s';"
-        % self._icon("%s.svg" % DiffUI.MODE_ICONS[self._mode])
-      )
-    html.append(
-      "let feedbackURL = '%s';"
-      % (
-        self._variables
-        and self._variables.resolve(self._variables.GLOBAL, "feedbackURL")
-        or ""
-      )
+      difficon = self._icon(f"{DiffUI.MODE_ICONS[self._mode]}.svg")
+      html.append(f"let diffIcon = '{difficon}';")
+    fburl = ""
+    if self._variables:
+      fburl = self._variables.resolve(self._variables.GLOBAL, "feedbackURL")
+    html.append(f"let feedbackURL = '{fburl or ''}';")
+    html.append(f"let themeDefault = '{themes.themes()[0][0]}';")
+    bwtheme = next(
+      t[0]
+      for t in themes.themes()
+      if "black" in t[0].lower() and "white" in t[0].lower()
     )
-    html.append("let themeDefault = '%s';" % themes.themes()[0][0])
+    html.append(f"let themeBW = '{bwtheme}';")
+    html.append(f"let themes = {json.dumps(themes.todict())};")
     html.append(
-      "let themeBW = '%s';"
-      % next(
-        t[0]
-        for t in themes.themes()
-        if "black" in t[0].lower() and "white" in t[0].lower()
-      )
-    )
-    html.append("let themes = %s;" % json.dumps(themes.todict()))
-    html.append(
-      open(os.path.join(srcdir, "diffui.js"), "r", encoding="utf-8").read()
+      open(os.path.join(srcdir, "diffui.js"), encoding="utf-8").read()
     )
     html.append("</script>")
     # Data
@@ -526,17 +503,15 @@ class DiffUI(object):
     The following encoded schematic may be proprietary to its author and all
     rights are reserved to that author unless expressly stated otherwise in the
     rendered schematic.\n*/""")
-    html.append(
-      "var data = '%s';"
-      % self._compress(json.dumps(self.schematic_index, sort_keys=True))
-    )
+    zindex = self._compress(json.dumps(self.schematic_index, sort_keys=True))
+    html.append(f"var data = '{zindex}';")
     if self._pages:
       html.append("var pageData = {")
       lib = Svg(header=False, auto_animate=False)
       lib.symbols = self._symbols
-      html.append("library: '%s'," % self._compress(repr(lib)))
+      html.append(f"library: '{self._compress(repr(lib))}',")
       for page in self._pages:
-        html.append("%s: '%s'," % (page.id, self._compress(repr(page.svg))))
+        html.append(f"{page.id}: '{self._compress(repr(page.svg))}',")
       html.append("};")
     html.append("</script>")
     html.append("</body></html>")
@@ -563,19 +538,19 @@ class DiffUI(object):
       self._uiprocess = subprocess.Popen(
         chrome
         + (
-          "--user-data-dir=%s" % self._tempdir,
+          f"--user-data-dir={self._tempdir}",
           "--renderer-process-limit=1",
           "--disable-extensions",
           "--no-proxy-server",
           "--disable-component-extensions-with-background-pages",
-          "--app=http://localhost:%u" % server.server_address[1],
+          f"--app=http://localhost:{server.server_address[1]}",
         ),
         shell=shell,
         stderr=subprocess.STDOUT,
         stdout=open(os.path.join(self._tempdir, "chrome.log"), "wb"),
       )
     except FileNotFoundError as e:
-      sys.stderr.write("ERROR: %s\n" % e)
+      sys.stderr.write(f"ERROR: {e}\n")
       server.shutdown()
       return 127
     ret = self._uiprocess.wait()

kischvidimer/__main__.py
@@ -26,8 +26,9 @@ def main(argv=sys.argv):
     print(f"USAGE: {argv[0]} COMMAND ...", file=sys.stderr)
     print("Recognized commands:", file=sys.stderr)
     for mod in sorted(mods):
-      if "def main(" in open(os.path.join(p, f"{mod}.py"), "r").read():
-        print(f"  {mod}", file=sys.stderr)
+      with open(os.path.join(p, f"{mod}.py")) as f:
+        if "def main(" in f.read():
+          print(f"  {mod}", file=sys.stderr)
     return 2
   mod = f".{argv[1]}"
   args = [f"{argv[0]} {argv[1]}"] + argv[2:]

kischvidimer/bmp.py
@@ -33,8 +33,6 @@ def getsize(d):
 
 def to_png(f):
   """Returns a Bytes() object of a PNG file"""
-  if isinstance(f, str):
-    f = open(f, "rb")
   if isinstance(f, io.IOBase):
     f = f.read()
 
@@ -60,16 +58,16 @@ def to_png(f):
   if header_size >= 40:
     compression = int.from_bytes(f[30:34], byteorder="little")
     if compression not in (0, 3):
-      raise Exception("compression (%d) not supported" % compression)
+      raise Exception(f"compression ({compression}) not supported")
     num_colors = (
       min(int.from_bytes(f[46:50], byteorder="little"), num_colors)
       or num_colors
     )
     if compression == 3:
       if bpp not in (16, 32):
-        raise Exception("invalid bit depth (%d) for bitmask" % bpp)
+        raise Exception(f"invalid bit depth ({bpp}) for bitmask")
       if header_size < 52 and header_size != 40:
-        raise Exception("bad header size (%d) for bitmask" % header_size)
+        raise Exception(f"bad header size ({header_size}) for bitmask")
       bitmask = tuple(
         int.from_bytes(f[i : i + 4], byteorder="little")
         for i in range(54, 54 + 4 * (3 if header_size < 56 else 4), 4)

kischvidimer/git.py
@@ -90,7 +90,7 @@ def cat(path, ref=":", relative=False):
       "git",
       "show",
       ":".join(
-        (ref, path if not relative or path.startswith("/") else "./%s" % path)
+        (ref, path if not relative or path.startswith("/") else f"./{path}")
       ),
     ],
     cwd=None if relative else repo_path(),
@@ -111,7 +111,7 @@ def cat_files(path, state=STATE_MODIFIED * 2):
     state[1] != STATE_DELETED,
   )
   return tuple(
-    cat(path, ":%s" % str(version)) if exists[version] else None
+    cat(path, f":{version}") if exists[version] else None
     for version in VERSIONS
   )
 
@@ -142,7 +142,7 @@ def get_version(repo=None):
     "git describe --all --always --broken --dirty --long".split(),
     cwd=repo or ".",
     capture_output=True,
-    universal_newlines=True,
+    text=True,
   )
   if ret.returncode == 0:
     ver = ret.stdout.strip()
@@ -203,10 +203,8 @@ def isdir(path_or_tuple, githash=None):
     path_or_tuple, githash = path_or_tuple
   if not githash or not is_in_repo(path_or_tuple):
     return os.path.isdir(path_or_tuple)
-  return (
-    True
-    if ls_tree(path_or_tuple + "/.", githash, full_tree=False, recurse=False)
-    else False
+  return bool(
+    ls_tree(path_or_tuple + "/.", githash, full_tree=False, recurse=False)
   )
 
 
@@ -218,11 +216,7 @@ def isfile(path_or_tuple, githash=None):
     return os.path.isfile(path_or_tuple)
   if isdir(path_or_tuple, githash):
     return False
-  return (
-    True
-    if ls_tree(path_or_tuple, githash, full_tree=False, recurse=False)
-    else False
-  )
+  return bool(ls_tree(path_or_tuple, githash, full_tree=False, recurse=False))
 
 
 def open_rb(path_or_tuple, githash=None):

kischvidimer/kicad_common.py
@@ -115,18 +115,12 @@ def rel_coord(gravity, rel=None, pos=None, vect=None):
     if pos is not None:
       ret = (rel[0] + ret[0], ret[1])
   elif gravity[0] == "r":
-    if pos is not None:
-      ret = (rel[2] - ret[0], ret[1])
-    else:
-      ret = (-ret[0], ret[1])
+    ret = (-ret[0], ret[1]) if pos is None else (rel[2] - ret[0], ret[1])
   if gravity[1] == "t":
     if pos is not None:
       ret = (ret[0], rel[1] + ret[1])
   elif gravity[1] == "b":
-    if pos is not None:
-      ret = (ret[0], rel[3] - ret[1])
-    else:
-      ret = (ret[0], -ret[1])
+    ret = (ret[0], -ret[1]) if pos is None else (ret[0], rel[3] - ret[1])
   return ret
 
 
@@ -312,9 +306,12 @@ class Effects(Modifier):
         rot += c["at"][0].rot(diffs, context) * (-1 if flipy != flipx else 1)
     rot = rot % 360
     spin = False
-    if rot in (0, 180) and inst_rot in (180, 270):
-      spin = True
-    elif rot in (90, 270) and inst_rot in (90, 180):
+    if (
+      rot in (0, 180)
+      and inst_rot in (180, 270)
+      or rot in (90, 270)
+      and inst_rot in (90, 180)
+    ):
       spin = True
     if inst_mirror and not (rot + inst_rot) % 180:
       spin = not spin
@@ -344,9 +341,8 @@ class Stroke(Modifier):
       stroke = tuple(self["color"][0].data)
       if any(stroke):
         args["color"] = stroke
-    if "type" in self:
-      if self["type"][0][0] != "default":
-        args["pattern"] = self["type"][0][0]
+    if "type" in self and self["type"][0][0] != "default":
+      args["pattern"] = self["type"][0][0]
     return args
 
 
@@ -824,7 +820,7 @@ def main(argv):
   for kwfile in argv[1:]:
     if not os.path.isfile(kwfile):
       continue
-    with open(kwfile, "r") as f:
+    with open(kwfile) as f:
       kws = sorted(line.strip() for line in f if line.strip())
     print(f"{kwfile}:")
     for kw in kws:

kischvidimer/kicad_pro.py
@@ -52,7 +52,7 @@ class KicadPro(Comparable):
     pgcount = sum(len(i) for i, _ in pages.values())
     variables.define(variables.GLOBAL, variables.PAGECOUNT, pgcount)
     context = self.context()
-    for filename, (instances, sch) in pages.items():
+    for _filename, (instances, sch) in pages.items():
       if p:
         p.write().incr()
       for path, sheet in instances:
@@ -128,7 +128,7 @@ class KicadPro(Comparable):
           file=sys.stderr,
         )
         return default_wks
-      wks = kicad_wks.kicad_wks(open(wks_path_expanded, "r"), wks_path_expanded)
+      wks = kicad_wks.kicad_wks(open(wks_path_expanded), wks_path_expanded)
     else:
       wks = kicad_wks.kicad_wks(
         git.open_rb(wks_path_expanded, rev), wks_path_expanded
@@ -225,7 +225,7 @@ def config_env_vars():
   for configdir in configdirs:
     for configfile in os.listdir(configdir):
       if configfile.lower() == "kicad_common.json":
-        config = json.load(open(os.path.join(configdir, configfile), "r"))
+        config = json.load(open(os.path.join(configdir, configfile)))
         envvars.update(config.get("environment", {}).get("vars", {}).items())
         break
   return envvars
@@ -242,7 +242,7 @@ def main(argv):
   path = argv[1] if len(argv) > 1 else None
   p = progress.Progress(sys.stderr)
   p.set_max(1).set_text(f"Loading {path or 'stdin'}").write()
-  with open(path, "r") if path else sys.stdin as f:
+  with open(path) if path else sys.stdin as f:
     proj = kicad_pro(f, path)
   p.incr()
   pages = proj.get_pages(None, None, p=p)

kischvidimer/kicad_sch.py
@@ -673,7 +673,7 @@ def main(argv):
   """
   s = svg.Svg(theme="default")
   path = argv[1] if len(argv) > 1 else None
-  with open(path, "r") if path else sys.stdin as f:
+  with open(path) if path else sys.stdin as f:
     data = sexp.parse(f.read())
   variables = Variables()
   data[0].fillvars(variables, [], None)

kischvidimer/kicad_sym.py
@@ -158,9 +158,12 @@ class PinDef(Drawable):
     # pin name should always be top/left, pin number below/right.
     # text should always be facing up/left
     swap_side = False
-    if rot in (0, 180) and inst_rot in (180, 270):
-      swap_side = True
-    elif rot in (90, 270) and inst_rot in (90, 180):
+    if (
+      rot in (0, 180)
+      and inst_rot in (180, 270)
+      or rot in (90, 270)
+      and inst_rot in (90, 180)
+    ):
       swap_side = True
     if inst_mirror and not (rot + inst_rot) % 180:
       swap_side = not swap_side
@@ -351,7 +354,7 @@ def main(argv):
   s = svg.Svg(theme="default")
   s.push_invert_y()
   path = argv[1] if len(argv) > 1 else None
-  with open(path, "r") if path else sys.stdin as f:
+  with open(path) if path else sys.stdin as f:
     data = sexp.parse(f.read())
   params = {
     "svg": s,

commit 44264107e8026e2dd9557137c2407d777bb3750d

Fix icon rendering
Broken by 8fb0aae536bb94c293d68af75ee943e9e626dc4b

kischvidimer/diffui.py
@@ -437,8 +437,15 @@ class DiffUI:
       data.setdefault("fill", None)
       data["src"] = None
       # Merge the SVG tag
-      inner = "".join(f' {k}="{v}"' for k, v in attrs(m[0], data).items() if v)
-      return re.sub(r"<svg\s[^>]*>", lambda m: f"<svg{inner}>", s)
+      return re.sub(
+        r"<svg\s[^>]*>",
+        lambda m: "".join(
+          ["<svg"]
+          + [f' {k}="{v}"' for k, v in attrs(m[0], data).items() if v]
+          + [">"]
+        ),
+        s,
+      )
 
     return re.sub(r"<img\s[^>]*>", repl, h)
 